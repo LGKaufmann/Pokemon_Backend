7ac9ea077ce961860660248e43b774f3e8d33c28
LGKaufmann <lautykaufmann76@gmail.com>
2024-01-23 21:41:39 -0300

    problemas de ndimiento

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const compression = require("compression");
 const pokemon = require("./routes/pokemon.js");
 const types = require("./routes/types.js");
 
@@ -12,6 +13,7 @@ const server = express();
 server.name = "API";
 
 server.disable("x-powered-by");
+server.use(compression());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
