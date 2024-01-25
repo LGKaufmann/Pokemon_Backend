b5658fa16130402f5250ffefd09b6bd0c5a5de9d
LGKaufmann <lautykaufmann76@gmail.com>
2024-01-25 03:38:08 -0300

    menos pokemones

--- utils/searchInApi.js
+++ utils/searchInApi.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const searchInApi = async () => {
-  const URL = "https://pokeapi.co/api/v2/pokemon?limit=600";
+  const URL = "https://pokeapi.co/api/v2/pokemon?limit=400";
   let arrayPokemonsApi = [];
 
   // carga de pokeAPI -----------------------------------------
